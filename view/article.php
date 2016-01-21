0c6072cf25c1915d74df25462de7efb2c2f1725a
siapran <siapran.candoris@gmail.com>
2016-01-21 11:58:32 +0100

    adjusted layout

--- view/article.php
+++ view/article.php
@@ -1,4 +1,4 @@
-<div class="col-md-4">
+<div class="col-md-6">
     <h1><a class="articleTitle" href=<?php echo 'article.php?id='.$article->art_id; ?>><?php echo $article->art_title; ?></a></h1>
     <p>
         <?php echo $article->art_content ?>
